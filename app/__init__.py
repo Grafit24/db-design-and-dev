e6d5102cf7d78ae01ce324f564028b48792408fa
Sergey Zelenovsky <zelnovskiygoodman454@gmail.com>
2023-04-16 22:40:09 +0300

    ADD: create_app and config file

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("config.Config")
+
+    db.init_app(app)
+    with app.app_context():
+        db.Model.metadata.reflect(db.engine)
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app

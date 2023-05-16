b3a6014aabe7a51603dd961cc466852fd0d84fbf
Sergey Zelenovsky <zelnovskiygoodman454@gmail.com>
2023-05-16 21:43:39 +0300

    ADD: Wrokspaces view

--- app/main.py
+++ app/main.py
@@ -12,3 +12,21 @@ def index():
 @login_required
 def profile():
     return render_template("profile.html", login=current_user.login)
+
+@main.route('/workspaces')
+@login_required
+def workspaces():
+    query = """
+    SELECT workspaces.id AS id, workspaces.workspace_name AS name, workspaces_users.is_owner AS is_owner 
+        FROM workspaces INNER JOIN workspaces_users INNER JOIN users 
+        ON workspaces_users.user_id = users.id ON workspaces.id = workspaces_users.id
+        WHERE users.login=%s;
+    """
+    login = current_user.login
+    result = db.engine.execute(query, login).fetchall()
+    return render_template("workspaces.html", query_result=result)
+
+@main.route("/workspaces-create")
+@login_required
+def create_workspace():
+    ...
--- app/models.py
+++ app/models.py
@@ -7,3 +7,16 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return self.DISTRICT
+    
+class Workspaces(db.Model, UserMixin):
+    __table__ = db.Model.metadata.tables['workspaces']
+
+    def __repr__(self):
+        return self.DISTRICT
+
+class WorkspacesUsers(db.Model, UserMixin):
+    """Connection between workspace 1-M users."""
+    __table__ = db.Model.metadata.tables['workspaces_users']
+
+    def __repr__(self):
+        return self.DISTRICT

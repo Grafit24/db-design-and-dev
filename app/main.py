095bfd78a4ab842828d4238e2b87fc2035cb15f8
Sergey Zelenovsky <zelnovskiygoodman454@gmail.com>
2023-05-17 19:40:03 +0300

    UPDATE: New workspaces

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, request, redirect
 from flask_login import login_required, current_user
 from . import db
+from .models import Workspaces, WorkspacesUsers
 
 main = Blueprint('main', __name__)
 
@@ -18,15 +19,42 @@ def profile():
 def workspaces():
     query = """
     SELECT workspaces.id AS id, workspaces.workspace_name AS name, workspaces_users.is_owner AS is_owner 
-        FROM workspaces INNER JOIN workspaces_users INNER JOIN users 
-        ON workspaces_users.user_id = users.id ON workspaces.id = workspaces_users.id
-        WHERE users.login=%s;
+        FROM workspaces INNER JOIN workspaces_users ON workspaces.id = workspaces_users.workspace_id
+        WHERE workspaces_users.user_id=%s;
     """
-    login = current_user.login
+    login = current_user.get_id()
     result = db.engine.execute(query, login).fetchall()
     return render_template("workspaces.html", query_result=result)
 
-@main.route("/workspaces-create")
+@main.route("/workspaces/create", methods=["POST"])
 @login_required
 def create_workspace():
+    workspace_name = request.form.get('workspace_name')
+    workspace = Workspaces(workspace_name=workspace_name)
+    
+    db.session.add(workspace)
+    db.session.flush()
+    db.session.refresh(workspace)
+
+    connect_admin = WorkspacesUsers(
+        workspace_id=workspace.id, 
+        user_id=current_user.get_id(), 
+        is_owner=True
+    )
+
+    db.session.add(connect_admin)
+    db.session.commit()
+
+    return redirect(url_for("main.workspaces"))
+
+
+@main.route("/workspaces/update", methods=["PUT"])
+@login_required
+def update_workspace():
+    ...
+
+
+@main.route("/workspaces/delete", methods=["DELETE"])
+@login_required
+def delete_workspace():
     ...
